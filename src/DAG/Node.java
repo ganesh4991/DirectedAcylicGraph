e0998e8582f9db33b94d435eb0b3d5589825f9ad
ganesh4991 <ganesh4991@gmail.com>
2016-05-21 03:52:53 -0400

    Final code with comments to Node

--- src/DAG/Node.java
+++ src/DAG/Node.java
@@ -2,10 +2,11 @@ package DAG;
 import java.util.ArrayList;
 import java.util.List;
 
+// Node class to store the contents of each point in the graph
 public class Node {
 	String name;
-	List<Node>out; 
-	List<Node>in;
+	List<Node>out; // List of outward edges
+	List<Node>in;	//list of inward edges
 	Node(String n){
 		name=n;
 		out=new ArrayList<Node>();
